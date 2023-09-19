748c2ea611655d82ba89c073cea5658003a93b29
mxrgxr <mgxrcias@gmail.com>
2023-09-18 20:18:27 -0700

    posts router

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :posts do
+    resources :comments, only: [:create, :destroy], module: :posts
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
