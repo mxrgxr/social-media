15bb5658a27cfeb3b05b7e2e482d0c449e7e9238
mxrgxr <mgxrcias@gmail.com>
2023-09-19 16:57:47 -0700

    form validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+  validates :title, length: { in: 5..40 }, presence: true
+  validates :body, length: { in: 10..100 }, presence: true
 end
