ccdae221baea9271edc4d8371e0747a96a419e05
mxrgxr <mgxrcias@gmail.com>
2023-09-20 12:23:47 -0700

    visual hierarchy nested comments

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,10 @@
 module CommentsHelper
+  def render_nested_comments(comments, level = 0)
+    comments.map do |comment|
+      content_tag(:div, class: "nested-comment", style: "margin-left: #{20 * level}px;") do
+        render(partial: "comments/comment", locals: { comment: comment }) +
+          (comment.replies.any? ? render_nested_comments(comment.replies, level + 1) : "")
+      end
+    end.join.html_safe
+  end
 end
