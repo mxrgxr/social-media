efce6c98e93a51134242a543bcd3d656268082c8
mxrgxr <mgxrcias@gmail.com>
2023-09-20 11:10:01 -0700

    delete post functionality

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, length: { in: 5..40 }, presence: true
   validates :body, length: { in: 10..100 }, presence: true
 end
